e9dbdeb186bf88b391f6d90bc1932ce5b81b25d7
chirag-memriya <chirag.memriya@matrixcomsec.com>
2023-02-08 16:09:11 +0530

    All the project

---
+++ app/src/main/java/com/example/clientapp/MainActivity.java
@@ -0,0 +1,72 @@
+package com.example.clientapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+
+public class MainActivity extends AppCompatActivity {
+    //Toolbar
+    Toolbar toolbar;
+
+    LinearLayout walletBtn;
+//    ImageButton walletBtn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        //Toolbar
+        toolbar=(Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        //        setSupportActionBar(toolbar);
+
+        //step -2
+        if(getSupportActionBar()!=null){
+//            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setTitle("Custom_Toolbar_Name");
+        }
+
+        //set toolbar title
+//        toolbar.setTitle("toolbarTitle");
+
+
+
+//        addVehicleBtn=findViewById(R.id.addVehicle);
+//        addVehicleBtn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent i=new Intent(MainActivity.this,AddVehicle.class);
+//                startActivity(i);
+//            }
+//        });
+
+
+
+
+
+        walletBtn=findViewById(R.id.wallet);
+        walletBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Intent i=new Intent(MainActivity.this,walletActivity.class);
+                Intent i=new Intent(MainActivity.this,walletActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+
+
+
+
+//    private void setSupportActionBar(Toolbar toolbar) {
+//    }
+//    public void startLog(View view){
+//        startActivity(new Intent(MainActivity.this, LogsActivity.class));
+//    }
+}
